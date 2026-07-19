aabfd61e53a24fa1b0aa4fd922bb112c62369fa7
boncabr <ariboncabe100@gmail.com>
2026-07-19 15:27:28 +0700

    Update video.js

--- src/commands/music/video.js
+++ src/commands/music/video.js
@@ -5,23 +5,24 @@ module.exports = {
     description: 'Memutar video dari link YouTube',
     voiceChannel: true,
 
-    async execute({ interaction }) {
+    async execute({ message, args }) {
         const player = useMainPlayer();
-        const url = interaction.options.getString('url');
+        const url = args[0];
         
-        if (!url) return interaction.reply('Harap masukkan URL video!');
+        if (!url) return message.reply('Harap masukkan URL video!');
 
-        const queue = player.nodes.create(interaction.guild, {
-            metadata: interaction.channel
+        const queue = player.nodes.create(message.guild, {
+            metadata: message.channel
         });
 
         try {
-            if (!interaction.member.voice.channel) await queue.connect(interaction.member.voice.channel);
-            const track = await player.search(url, { requestedBy: interaction.user });
+            if (!message.member.voice.channel) return message.reply('Kamu harus berada di voice channel!');
+            await queue.connect(message.member.voice.channel);
+            const track = await player.search(url, { requestedBy: message.author });
             await queue.play(track.tracks[0]);
-            await interaction.reply(`Memutar video: ${track.tracks[0].title}`);
+            await message.reply(`Memutar video: ${track.tracks[0].title}`);
         } catch (e) {
-            await interaction.reply('Gagal memutar video.');
+            await message.reply('Gagal memutar video.');
         }
     },
 };
