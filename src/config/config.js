81d147f28315c70d486423be2e5d8640b6dd0561
boncabr <ariboncabe100@gmail.com>
2026-08-07 19:35:24 +0700

    Increase default maxDuration to 8 hours (28800000 ms)

--- src/config/config.js
+++ src/config/config.js
@@ -34,7 +34,7 @@ module.exports = {
   music: {
     defaultVolume: parseInt(process.env.DEFAULT_VOLUME || '100'),
     maxQueueSize: 500,
-    maxDuration: parseInt(process.env.MAX_DURATION || '7200000'),
+    maxDuration: parseInt(process.env.MAX_DURATION || '28800000'),
     searchPlatform: 'ytsearch',
     leaveOnEmptyDelay: 30000,
     leaveOnEndDelay: 30000,
