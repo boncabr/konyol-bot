91225493dd6268961ab5fe12ba7c077ade1578cd
boncabr <ariboncabe100@gmail.com>
2026-08-20 17:57:32 +0700

    Update config.js

--- artifacts/discord-bot/src/config/config.js
+++ artifacts/discord-bot/src/config/config.js
@@ -46,12 +46,12 @@ module.exports = {
   },
 
   music: {
-    defaultVolume: parseInt(process.env.DEFAULT_VOLUME || '80'),
+    defaultVolume: parseInt(process.env.DEFAULT_VOLUME || '100'),
     maxQueueSize: 500,
     searchPlatform: 'ytmsearch',
     leaveOnEmptyDelay: 30000,
     leaveOnEndDelay: 30000,
-    voiceChannelBitrate: 320000,
+    voiceChannelBitrate: 384000,
   },
 
   cooldowns: {
